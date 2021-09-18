8dd42c14220a71a5f441b2fa2f71bccd3dec4955
Taili55 <39952943+Taili55@users.noreply.github.com>
2021-09-18 09:46:27 -0300

    Update parpython.py

--- parpython.py
+++ parpython.py
@@ -4,12 +4,12 @@ soma=1
 ini = time.time()
 while (contador <= 100000):
     if (contador % 2) == 0:
-     contador= contador + 1
      soma= soma +1 
-print("Resultado =  ",soma)
+    
+    contador= contador + 1
+print("Resultado =  ", soma)
 
 fim = time.time()
-
 print("Tempo de execucao:  ", fim - ini)
 
 
